20ca75628c732d91009af80b825e4a86591cc99e
H Kilroy <henry.kilroy@vtsd.com>
2025-12-10 13:36:31 -0500

    meowl

--- 1.2.1/kiedesdghamne.py
+++ 1.2.1/kiedesdghamne.py
@@ -3,35 +3,32 @@ import turtle as trtl
 import random as rand
 
 #-----game configuration----
+spot_color = "purple"
 score = 0
-font_setup = ("Papyrus", 20, "normal")
-wn = trtl.Screen()
-wn.register_shape("meowl.gif")
-wn.bgcolor("Green")
-timer = 30
+font_setup = ("Arial", 20, "normal")
+timer = 5
 counter_interval = 1000   #1000 represents 1 second
 timer_up = False
+
 #-----initialize turtle-----
 # Score turtle
 score_writer = trtl.Turtle()
 score_writer.penup()
-score_writer.hideturtle()
+counter =  trtl.Turtle()
+counter.penup()
 score_writer.speed(0)
-# Box turtle
+counter.speed(0)
+# Turtle to draw a box
 box_turtle = trtl.Turtle()
 box_turtle.penup()
-box_turtle.hideturtle()
-box_turtle.speed(0)
+
 # Shape turtle
 meowl = trtl.Turtle()
-meowl.shape("meowl.gif")
+meowl.shape("circle")
+meowl.color(spot_color)
+meowl.shapesize(3)
 meowl.penup()
 meowl.speed(0)
-
-# Counter turtle
-counter =  trtl.Turtle()
-counter.speed(0)
-counter.hideturtle()
 #-----game functions--------
 # Draw the box for the score
 def scoreBox():
@@ -48,8 +45,11 @@ def scoreBox():
 
     # Place score_writer where it will write the score
     score_writer.penup()
-    score_writer.goto(300, 325)
+    score_writer.goto(300, 332)
 
+    # Hide the turtles
+    score_writer.hideturtle()
+    box_turtle.hideturtle()
 
 # Get a score boost, move the turtle randomly
 def spot_clicked(x, y):
@@ -71,52 +71,34 @@ def update_score():
     global score
     # Increment the score by 1
     score += 1
-    # Clear prior score
+    # Clear out the prior score
     score_writer.clear()
-    # print the score
+    # Print the current score
     score_writer.write(score, font=font_setup)
 
-def update_timer():
-    counter.penup()
-    counter.goto(-300, 325)
-    global timer, timer_up
-    counter.clear()
-    if timer <= 0:
-        counter.write("Time's Up", font=font_setup)
-        timer_up = True
-    else:
-        counter.write("Timer: " + str(timer), font=font_setup)
-        timer -= 1
-        counter.getscreen().ontimer(update_timer, counter_interval)
+# Counter setup
+def counter_setup():
+    counter.forward(-200)
+    counter.left(90)
+    counter.forward(300)
+    counter.right(90)
+    counter.hideturtle()
 
-
-
+# Start the countown and update it each frame
+def countdown():
+  global timer, timer_up
+  counter.clear()
+  if timer <= 0:
+    counter.write("Time's Up", font=font_setup)
+    timer_up = True
+  else:
+    counter.write("Timer: " + str(timer), font=font_setup)
+    timer -= 1
+    counter.getscreen().ontimer(countdown, counter_interval)
 #-----events----------------
 meowl.onclick(spot_clicked)
-update_timer()
-
+counter_setup()
 scoreBox()
+wn = trtl.Screen()
+wn.ontimer(countdown, counter_interval)
 wn.mainloop()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
