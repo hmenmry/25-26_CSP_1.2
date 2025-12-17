8dc00791e0573d396a59ccfdabc042684dc62e42
H Kilroy <henry.kilroy@vtsd.com>
2025-12-17 13:08:57 -0500

    meowl

---
+++ 1.2.2/meowlgame.py
@@ -0,0 +1,119 @@
+#-----import statements-----
+import turtle as trtl
+import random as rand
+import leaderboard as lb
+#-----game configuration----
+
+score = 0
+font_setup = ("Papyrus", 20, "normal")
+wn = trtl.Screen()
+wn.register_shape("meowl.gif")
+wn.bgcolor("Green")
+timer = 30
+counter_interval = 1000   #1000 represents 1 second
+timer_up = False
+#-----initialize turtle-----
+leaderboard_file_name = "a122_leaderboard.txt"
+player_name = input("whats your name twin ")
+# Score turtle
+score_writer = trtl.Turtle()
+score_writer.penup()
+score_writer.hideturtle()
+score_writer.speed(0)
+# Box turtle
+box_turtle = trtl.Turtle()
+box_turtle.penup()
+box_turtle.hideturtle()
+box_turtle.speed(0)
+# Shape turtle
+meowl = trtl.Turtle()
+meowl.shape("meowl.gif")
+meowl.penup()
+meowl.speed(0)
+
+# Counter turtle
+counter =  trtl.Turtle()
+counter.speed(0)
+counter.hideturtle()
+#-----game functions--------
+# Draw the box for the score
+def scoreBox():
+    # Set up the starting location and pendown
+    box_turtle.goto(275, 325)
+    box_turtle.pendown()
+
+    # Draw the box
+    for sides in range(2):
+        box_turtle.forward(100)
+        box_turtle.left(90)
+        box_turtle.forward(50)
+        box_turtle.left(90)
+
+    # Place score_writer where it will write the score
+    score_writer.penup()
+    score_writer.goto(300, 325)
+
+
+# Get a score boost, move the turtle randomly
+def spot_clicked(x, y):
+    global timer_up
+    if timer_up == False:
+        change_position()
+    else:
+        meowl.hideturtle()
+
+def change_position():
+    # Move the turtle to a random location
+    newX = rand.randint(-300, 300)
+    newY = rand.randint(-300, 300)
+    meowl.goto(newX, newY)
+    update_score()
+
+def update_score():
+    # Include the global score
+    global score
+    # Increment the score by 1
+    score += 1
+    # Clear prior score
+    score_writer.clear()
+    # print the score
+    score_writer.write(score, font=font_setup)
+
+def update_timer():
+    counter.penup()
+    counter.goto(-300, 325)
+    global timer, timer_up
+    counter.clear()
+    if timer <= 0:
+        counter.write("Time's Up", font=font_setup)
+        timer_up = True
+        manage_leaderboard()
+    else:
+        counter.write("Timer: " + str(timer), font=font_setup)
+        timer -= 1
+        counter.getscreen().ontimer(update_timer, counter_interval)
+
+
+# manages the leaderboard for top 5 scorers
+def manage_leaderboard():
+
+  global score
+  global meowl
+
+  # get the names and scores from the leaderboard file
+  leader_names_list = lb.get_names(leaderboard_file_name)
+  leader_scores_list = lb.get_scores(leaderboard_file_name)
+
+  # show the leaderboard with or without the current player
+  if (len(leader_scores_list) < 5 or score >= leader_scores_list[4]):
+    lb.update_leaderboard(leaderboard_file_name, leader_names_list, leader_scores_list, player_name, score)
+    lb.draw_leaderboard(True, leader_names_list, leader_scores_list, meowl, score)
+
+  else:
+    lb.draw_leaderboard(False, leader_names_list, leader_scores_list, meowl, score)
+#-----events----------------
+meowl.onclick(spot_clicked)
+update_timer()
+
+scoreBox()
+wn.mainloop()
---
+++ parampractice/example 1.py
@@ -0,0 +1,6 @@
+# Method to add numbers
+def add_numbers(one, two):
+    sum = one + two
+    return sum
+
+print(add_numbers(6,7))
