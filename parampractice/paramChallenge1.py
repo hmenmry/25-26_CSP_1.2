0fe9aee3213e6c255dfbbc49aac39fe072f48fd1
H Kilroy <henry.kilroy@vtsd.com>
2026-01-14 13:09:37 -0500

    meowl

---
+++ parampractice/paramChallenge1.py
@@ -0,0 +1,26 @@
+# Make 3 methods following the details given here.
+
+# Method 1: name it "tic" and it takes 2 parameters "num1" and "num2" and returns
+# the value of the 2 parameters when subtracted (ie: num1 - num2)
+def tic(one, two):
+    difference = one - two
+    return difference
+
+# Method 2: name it "tac" and it takes 1 parameter "exp"
+# use a loop to multiply the number 5 by itself "exp" times
+# return that value
+def tac(exp):
+    for meowl in range(exp):
+        product = 5 ** exp
+        return product
+
+# Method 3: name it "toe" that takes no parameters.  This method should print
+# the results of a method call to "tic(3, 5)" and "toe(4)" to the console
+def toe():
+    print(tic(3, 5),tac(4))
+
+toe()
+
+
+
+
